4035771587af4d46eb923bb3bbd210fb74d84f94
Benja153 <goosta19802@gmail.com>
2023-04-28 17:47:16 -0300

    Se agrego principal.css y se agregaron 4 imagenes

--- src/Components/Principal.jsx
+++ src/Components/Principal.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
-
+import ImgLogo from '../Img/logo512.png'
+import '../Styles/principal.css'
 const Principal = () => {
+
   return (
-    <div>Este es el contenido Principal</div>
+    <>
+    <div>Este es el contenido Principal el numero es</div>
+    <img
+        className='img1-negro'
+        src={ImgLogo}
+    ></img>
+     <img
+        className='img1-rojo'
+
+        src={ImgLogo}
+    ></img>
+     <img
+        src={ImgLogo}
+    ></img>
+     <img
+        className='img1-verde'
+     
+        src={ImgLogo}
+    ></img>
+    </>
+
   )
 }
 
